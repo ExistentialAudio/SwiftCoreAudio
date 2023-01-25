b99417be013baebc3e31a39c2830d7d925f39777
Devin Roth <devin@devinrothmusic.com>
2023-01-25 14:23:36 -0800

    Rewrite

---
+++ Sources/SwiftCoreAudio/AudioStream.swift
@@ -0,0 +1,51 @@
+//
+//  AudioStream.swift
+//  VSXSystemwide
+//
+//  Created by Devin Roth on 2023-01-10.
+//
+
+import Foundation
+import CoreAudio
+
+class AudioStream {
+    public var audioObjectID: AudioObjectID
+    
+    public var direction: AudioDirection {
+        get {
+            
+            var audioObjectPropertyAddress = AudioObjectPropertyAddress(mSelector: kAudioStreamPropertyDirection, mScope: 0, mElement: 0)
+            var dataSize = UInt32(MemoryLayout<UInt32>.stride)
+            var data = UInt32()
+            
+            let status = AudioObjectGetPropertyData(audioObjectID, &audioObjectPropertyAddress, 0, nil, &dataSize, &data)
+            
+            guard status == noErr else {
+                return .unknown
+            }
+            
+            return AudioDirection(value: data)
+        }
+    }
+    
+    init(audioObjectID: AudioObjectID) {
+        self.audioObjectID = audioObjectID
+    }
+}
+
+public enum AudioDirection: Int {
+    case input = 1
+    case output = 0
+    case unknown = -1
+    
+    init(value: UInt32) {
+        switch value {
+        case 0:
+            self = .output
+        case 1:
+            self = .input
+        default:
+            self = .unknown
+        }
+    }
+}
